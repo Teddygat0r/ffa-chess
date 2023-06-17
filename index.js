ff167a2afd09f5887b74e08c5713d1b87088c172
RacketeerGames <zhy0224@gmail.com>
2023-06-17 12:36:01 -0700

    Setup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
