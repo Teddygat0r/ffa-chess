6da5779a511b07a7ce24277c12f4501eabbad113
Teddygat0r <zhy0224@gmail.com>
2023-06-22 18:24:09 -0700

    Version1

--- index.js
+++ index.js
@@ -2,8 +2,70 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { Chess } = require("chess.js");
+let gameData = [1, 0, 0, 0];
+let chess = new Chess();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+const reset = () => {
+    chess = new Chess();
+    gameData[0]++;
+    io.emit("setup", {
+        fen: chess.fen(),
+        turn: chess.turn(),
+        moves: chess.moves({ verbose: true }),
+        history: chess.history({ verbose: true }),
+        gameData: gameData,
+    });
+    
+}
+
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.emit("setup", {
+        fen: chess.fen(),
+        turn: chess.turn(),
+        moves: chess.moves({ verbose: true }),
+        history: chess.history({ verbose: true }),
+        gameData: gameData,
+    });
+
+    socket.on("move", (data) => {
+        try {
+            const move = chess.move(data);
+            if (chess.isCheckmate()) {
+                io.emit("checkmate", {
+                    fen: chess.fen(),
+                    win: chess.turn(),
+                    moves: chess.moves({ verbose: true }),
+                });
+                gameData[1 + (chess.turn() === "b" ? 0 : 1)]++;
+                reset();
+
+            } else if (chess.isDraw()) {
+                io.emit("draw", {
+                    fen: chess.fen(),
+                    moves: chess.moves({ verbose: true }),
+                });
+                gamedata[3]++;
+                reset();
+            } else {
+                io.emit("boardUpdate", {
+                    fen: chess.fen(),
+                    turn: chess.turn(),
+                    moves: chess.moves({ verbose: true }),
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            socket.emit("illegalMove");
+        }
+    });
+});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
--- public/index.js
+++ public/index.js
@@ -1,6 +1,162 @@
+const socket = io();
+let turn = "w";
+let moves = [];
+let history = [];
+let currentTurn = 0;
+let allowMoves = true;
+let gameData = [];
 
-var board1 = Chessboard('board1', {
+const addOverlay = (square) => {
+    $(".square-" + square).prepend('<div class="overlay"></div>');
+};
+const removeOverlay = () => {
+    $("div.overlay").remove();
+};
+
+const onMouseoverSquare = (square, piece) => {
+    removeOverlay();
+    if (allowMoves)
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].from === square) {
+                addOverlay(moves[i].to);
+            }
+        }
+};
+
+const onMouseoutSquare = () => {
+    removeOverlay();
+};
+
+const onDragStart = (source, piece, position, orientation) => {
+    // only pick up pieces for the side to move
+    if (!allowMoves) return false;
+    if (
+        (turn === "w" && piece.search(/^b/) !== -1) ||
+        (turn === "b" && piece.search(/^w/) !== -1)
+    ) {
+        return false;
+    }
+};
+
+const onDrop = (source, target) => {
+    // see if the move is legal
+    socket.emit("move", {
+        from: source,
+        to: target,
+        promotion: "q", // NOTE: always promote to a queen for example simplicity
+    });
+};
+
+const config = {
     draggable: true,
-    dropOffBoard: 'trash',
-    sparePieces: true
+    position: "start",
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
+};
+
+const downloadFen = () => {
+    let ts = "";
+    for (let i = 0; i < history.length; i++) {
+        ts += history[i] + "\n";
+    }
+
+    const element = document.createElement("a");
+    element.setAttribute(
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(ts)
+    );
+    element.setAttribute("download", `FEN-${ gameData[0] }-${ gameData[1] }-${ gameData[2] }-${ gameData[3] }-${ history.length }.txt`);
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+};
+
+const board1 = Chessboard("board1", config);
+
+const setBoard = (t) => {
+    allowMoves = t === history.length - 1;
+    board1.position(history[t]);
+};
+
+const setGameData = () => {
+    document.getElementById("label").innerText = `Game #${gameData[0]}`;
+    document.getElementById(
+        "wins"
+    ).innerText = `White: ${gameData[1]} Wins  | Draw: ${gameData[3]} Games | Black: ${gameData[2]} Wins`;
+};
+
+const getPreviousBoard = () => {
+    if (currentTurn > 0) {
+        currentTurn--;
+        setBoard(currentTurn);
+    }
+};
+
+const getNextBoard = () => {
+    if (currentTurn < history.length - 1) {
+        currentTurn++;
+        setBoard(currentTurn);
+    }
+};
+
+const getFirstBoard = () => {
+    currentTurn = 0;
+    setBoard(currentTurn);
+};
+
+const getLastBoard = () => {
+    currentTurn = history.length - 1;
+    setBoard(currentTurn);
+};
+
+socket.on("setup", (data) => {
+    turn = data.turn;
+    moves = data.moves;
+    history = [];
+    for (let i = 0; i < data.history.length; i++) {
+        history.push(data.history[i].before);
+    }
+    history.push(data.fen);
+    currentTurn = history.length - 1;
+    setBoard(currentTurn);
+    gameData = data.gameData;
+    setGameData();
+});
+
+socket.on("boardUpdate", (data) => {
+    history.push(data.fen);
+    if (currentTurn == history.length - 2) {
+        currentTurn++;
+        setBoard(currentTurn);
+    }
+    turn = data.turn;
+    moves = data.moves;
+});
+
+socket.on("illegalMove", () => {
+    setBoard(currentTurn);
+});
+
+socket.on("checkmate", (data) => {
+    history.push(data.fen);
+    if (currentTurn == history.length - 2) {
+        currentTurn++;
+        setBoard(currentTurn);
+    }
+
+    alert(`${data.win === "w" ? "White" : "Black"} in Checkmate!`);
+});
+socket.on("draw", (data) => {
+    history.push(data.fen);
+    if (currentTurn == history.length - 2) {
+        currentTurn++;
+        setBoard(currentTurn);
+    }
+    alert("Draw");
 });
